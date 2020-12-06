bfa4be25f250590776abc41b3709b292cb9f9e6e
avi-verma-web <imavinashverma99@gmail.com>
2020-12-06 22:38:09 +0530

    saving to github

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
     <h1>HOW TO DEPLOY AND UPDATE DEPLOYED SITE ON NETLIFY..</h1>
+    <h2>UPDATION PART</h2>
+    <p>Now we will update the website which is already deployed.</p>
     </div>
   );
 }
