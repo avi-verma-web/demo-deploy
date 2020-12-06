be82e1e212fd1ff8352666e23a0c21410a149de5
avi-verma-web <imavinashverma99@gmail.com>
2020-12-06 22:32:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+    <h1>HOW TO DEPLOY AND UPDATE DEPLOYED SITE ON NETLIFY..</h1>
+    </div>
+  );
+}
+
+export default App;
