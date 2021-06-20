947411ae368940b84f4bce4b37b2d9d054361eda
avi-verma-web <imavinashverma99@gmail.com>
2021-06-20 18:27:06 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <h1>HOW TO DEPLOY AND UPDATE DEPLOYED SITE ON NETLIFY..</h1>
     <h2>UPDATION PART</h2>
     <p>Now we will update the website which is already deployed.</p>
+  
     </div>
   );
 }
